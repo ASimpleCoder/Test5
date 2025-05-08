63c8acc4460065de8b65d9d7bea59886a2d852b7
LlamaCodrr950 <163625693+ASimpleCoder@users.noreply.github.com>
2025-05-08 16:11:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import os
+import smtplib as smtp
+
+sender = smtp.SMTP("smtp.gmail.com")
+sender.starttls()
+sender.login(os.environ("password"), os.environ("email"))
 
 st.title("SparkRide Carwash Easy Bookings")
 st.subheader("Book A Variety Of Car Washes and Get them in just a day")
@@ -7,6 +13,7 @@ st.write("*To book instant car washes (washes in under a hour), please call +91 
 st.subheader("Book a car wash the for a set day")
 date = st.date_input("Day Of Carwash: ", format="DD/MM/YYYY")
 time = st.time_input("Time Of Carwash: ")
+parking = st.text_input("Enter Parking Lot Number: ")
 
 st.subheader("Select Package")
 selected = st.radio("Select a option to wash your car", ["Simple Microfiber Rub - ₹100", "Microfiber rub with water, soap - ₹200", "Hardcore scrub - ₹250"])
@@ -14,4 +21,4 @@ selected = st.radio("Select a option to wash your car", ["Simple Microfiber Rub 
 submit_button = st.button("Submit a booking")
 
 if submit_button:
-  st.write(selected)
+  sender.sendmail(os.environ("password"), os.environ("reciever"), f"NEW BOOKING ARRIVED \n\n Day: {date}, Time: {time}, Parking Lot Number: {parking} \n Mode: {selected}")
